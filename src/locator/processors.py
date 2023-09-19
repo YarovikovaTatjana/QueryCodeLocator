58300eeabd7078e7ac229e289cfdd4aa03cd8a61
yarovikova.tatyana <yarovikova.tatyana@otr.ru>
2023-09-19 17:44:35 +0300

    добавление поиска по другим типам дескрипторов, добавление файла настроек с возможностью настройки ветки и тех файлов, которые нужны

--- src/locator/processors.py
+++ src/locator/processors.py
@@ -1,4 +1,5 @@
 import base64
+import configparser
 import queue
 import re
 import threading
@@ -28,13 +29,13 @@ class FileProducer(threading.Thread):
     @:param folderQueue Queue for find folder path
     """
 
-    def __init__(self, project: Project, commit, pipeline: queue.Queue, folderQueue: queue.Queue, regex):
+    def __init__(self, project: Project, commit, pipeline: queue.Queue, folder: queue.Queue, regex):
         super().__init__()
         self.uuid = str(uuid.uuid4())
         self.project = project
         self.pipeline = pipeline
         self.commit = commit
-        self.folderQueue = folderQueue
+        self.folder = folder
         self.pattern = re.compile(regex, re.IGNORECASE | re.VERBOSE)
 
     """
@@ -49,15 +50,15 @@ class FileProducer(threading.Thread):
             path = file['path']
             name = file['name']
             if file['type'] == 'tree':
-                self.folderQueue.put(path)
+                self.folder.put(path)
             elif key and self.pattern.search(name):
                 self.pipeline.put({'id': file['id'], 'name': name, 'path': path})
 
     # Bypass all folders
     def run(self):
         print(f'run File Producer-{self.uuid}')
-        while not self.folderQueue.empty():
-            value = self.folderQueue.get()
+        while not self.folder.empty():
+            value = self.folder.get()
             self.bypass(value)
 
         self.pipeline.put(FINISH)
@@ -115,7 +116,7 @@ class SQLJavaProcessorConsumer(threading.Thread):
         print(f'consumer-{self.uuid} завершает свою работу')
 
 
-class SQLXMLProcessorConsumer(threading.Thread):
+class SQLLCProcessorConsumer(threading.Thread):
     def __init__(self, project: Project, pipeline: queue.Queue, out: queue.Queue, attempts):
         super().__init__()
         self.uuid = str(uuid.uuid4())
@@ -155,7 +156,7 @@ class SQLXMLProcessorConsumer(threading.Thread):
         return result
 
     def run(self):
-        print(f'run xml consumer-{self.uuid}')
+        print(f'run lc consumer-{self.uuid}')
         file = None
         while file != FINISH:
             time.sleep(0.5)
@@ -163,7 +164,7 @@ class SQLXMLProcessorConsumer(threading.Thread):
                 file = self.pipeline.get()
                 if file is FINISH:
                     print(f'Принято сообщение о завершении записи\n'
-                          f'Читатель xml consumer-{self.uuid}, осталось попыток {self.attempts}\n'
+                          f'Читатель lc consumer-{self.uuid}, осталось попыток {self.attempts}\n'
                           f'В очереди {self.pipeline.qsize()}')
                     self.pipeline.put(FINISH)  # pushing further
                     if self.attempts > 0:
@@ -176,12 +177,203 @@ class SQLXMLProcessorConsumer(threading.Thread):
                 try:
                     result = self.file_processor(textFile.encode('utf-8'))
                     for r in result:
-                        self.out.put({"path": file['path'], "sql": r['sql'], "id": r['id'], "description": r['description']})
+                        self.out.put(
+                            {"path": file['path'], "sql": r['sql'], "id": r['id'], "description": r['description']})
                 except Exception as ex:
                     print(f'an error {ex} was encountered while parsing the file {path}')
 
         self.out.put('finish')
-        print(f'consumer-{self.uuid} завершает свою работу')
+        print(f'lc-consumer-{self.uuid} завершает свою работу')
 
 
+class SQLDescProcessorConsumer(threading.Thread):
+    def __init__(self, project: Project, pipeline: queue.Queue, out: queue.Queue, attempts):
+        super().__init__()
+        self.uuid = str(uuid.uuid4())
+        self.project = project
+        self.pipeline = pipeline
+        self.out = out
+        self.attempts = attempts
 
+    @staticmethod
+    def file_check_processor(file):
+        result = []
+        xml_tree = ET.ElementTree(ET.fromstring(file))
+        root = xml_tree.getroot()
+        # Primary display of tags
+        refs = [item for item in root.iter() if item.tag.endswith('checkGroup')]
+        # Sorting out potentially suitable
+        for ref in refs:
+            checks = [item for item in ref.getchildren() if item.tag.endswith("check")]
+            if checks.__len__() == 0:
+                continue
+            for check in checks:
+                parameters = [item for item in check.getchildren() if item.tag.endswith("param")]
+                if parameters.__len__() == 0:
+                    continue
+                for parameter in parameters:
+                    name = parameter.attrib['name']
+                    try:
+                        if name in dictionary:
+                            continue
+                        value = parameter.text
+                        if value is None:
+                            continue
+                        if (parameter.attrib['name'] == 'sql') or ('fields.' in value):
+                            result.append({"sql": value, "id": check.attrib['id'], "desc": check.attrib['desc']})
+                    except KeyError as error:
+                        print(f'parameter parsing error {error} with name {name}')
+        return result
+
+    @staticmethod
+    def file_table_processor(file):
+        result = []
+        xml_tree = ET.ElementTree(ET.fromstring(file))
+        root = xml_tree.getroot()
+        # Primary display of tags
+        refs = [item for item in root.iter() if item.tag.endswith('action')]
+        # Sorting out potentially suitable
+        for ref in refs:
+            parameters = [item for item in ref.getchildren() if item.tag.endswith("proc")]
+            descr = [item for item in ref.getchildren() if item.tag.endswith("comment")]
+            if parameters.__len__() == 0:
+                continue
+            for parameter in parameters[0].getchildren():
+                name = parameter.attrib['name']
+                try:
+                    if name in dictionary:
+                        continue
+                    value = parameter.text
+                    if value is None:
+                        continue
+                    if descr.__len__() > 0:
+                        desc = descr[0].text
+                    else:
+                        desc = "-"
+                    if (parameter.attrib['name'] == 'sql') or ('fields.' in value):
+                        result.append({"sql": value, "id": ref.attrib['id'], "desc": desc})
+                except KeyError as error:
+                    print(f'parameter parsing error {error} with name {name}')
+        return result
+
+    @staticmethod
+    def file_fill_processor(file):
+        result = []
+        xml_tree = ET.ElementTree(ET.fromstring(file))
+        root = xml_tree.getroot()
+        # Primary display of tags
+        refs = [item for item in root.iter() if item.tag.endswith('procedure')]
+        if refs.__len__() == 0:
+            return result
+        # Sorting out potentially suitable
+        for ref in refs:
+            for parameter in ref.getchildren():
+                name = parameter.attrib['name']
+                try:
+                    if name in dictionary:
+                        continue
+                    value = parameter.text
+                    if value is None:
+                        continue
+                    try:
+                        desc = ref.attrib["desc"]
+                    except KeyError as error:
+                        desc = ref.attrib["bean"] + "." + ref.attrib["method"]
+                    if (parameter.attrib['name'] == 'sql') or ('fields.' in value):
+                        result.append({"sql": value, "id": ref.attrib['field'], "desc": desc})
+                except KeyError as error:
+                    print(f'parameter parsing error {error} with name {name}')
+        return result
+
+    @staticmethod
+    def file_edit_processor(file):
+        result = []
+        xml_tree = ET.ElementTree(ET.fromstring(file))
+        root = xml_tree.getroot()
+        # Primary display of tags
+        refs = [item for item in root.iter() if item.tag.endswith('actions')]
+        # Sorting out potentially suitable
+        for ref in refs:
+            actions = [item for item in ref.getchildren() if item.tag.endswith("action")]
+            if actions.__len__() == 0:
+                continue
+            for action in actions:
+                parameters = [item for item in action.getchildren() if item.tag.endswith("proc")]
+                descr = [item for item in action.getchildren() if item.tag.endswith("comment")]
+                if parameters.__len__() == 0:
+                    continue
+                for parameter in parameters[0].getchildren():
+                    name = parameter.attrib['name']
+                    try:
+                        if name in dictionary:
+                            continue
+                        value = parameter.text
+                        if value is None:
+                            continue
+                        if descr.__len__() == 0:
+                            desc = "-"
+                        else:
+                            desc = descr[0].text
+                        if (parameter.attrib['name'] == 'sql') or 'fields.' in value:
+                            result.append({"sql": value, "id": action.attrib['id'], "desc": desc})
+                    except KeyError as error:
+                        print(f'parameter parsing error {error} with name {name}')
+        return result
+
+    def run(self):
+        print(f'run desc consumer-{self.uuid}')
+        file = None
+        while file != FINISH:
+            time.sleep(0.75)
+            while not self.pipeline.empty():
+                file = self.pipeline.get()
+                if file is FINISH:
+                    print(f'Принято сообщение о завершении записи\n'
+                          f'Читатель desc consumer-{self.uuid}, осталось попыток {self.attempts}\n'
+                          f'В очереди {self.pipeline.qsize()}')
+                    self.pipeline.put(FINISH)  # pushing further
+                    if self.attempts > 0:
+                        self.attempts -= 1
+                    else:
+                        break
+                fileB64 = self.project.repository_blob(file['id'])
+                textFile = base64.b64decode(fileB64['content']).decode()
+                path = file['path']
+                section = "default"
+                if '.edit' in path:
+                    section = "EDIT"
+                elif '.table-actions' in path:
+                    section = "TABLE"
+                elif '.check' in path:
+                    section = "CHECK"
+                elif '.fill' in path:
+                    section = "FILL"
+                if section == "default":
+                    continue
+
+                try:
+                    if section == "EDIT":
+                        result = self.file_edit_processor(textFile.encode('utf-8'))
+                    elif section == "TABLE":
+                        result = self.file_table_processor(textFile.encode('utf-8'))
+                    elif section == "FILL":
+                        result = self.file_fill_processor(textFile.encode('utf-8'))
+                    else:
+                        result = self.file_check_processor(textFile.encode('utf-8'))
+                    if result.__len__() == 0:
+                        continue
+                    try:
+                        for r in range(len(result)):
+                            sql = result[r]['sql']
+                            id = result[r]['id']
+                            desc = result[r]['desc']
+                            try:
+                                self.out.put({"path": path, "sql": sql, "id": id, "description": desc})
+                            except Exception as e:
+                                print(f'an error {e} ошибка добавления в out параметров из файла {path}')
+                    except Exception as ex:
+                        print(f'an error {ex} ошибка заполнения out')
+                except Exception as ex:
+                    print(f'an error {ex} was encountered while parsing the file {path}')
+        self.out.put('finish')
+        print(f'desc-consumer-{self.uuid} завершает свою работу')
--- src/main.py
+++ src/main.py
@@ -1,19 +1,134 @@
 import queue
+import configparser
 from sys import argv
 
 from src.connection.gitlab_conn import Connector
-from src.locator.processors import FileProducer, SQLJavaProcessorConsumer, SQLXMLProcessorConsumer
-from src.locator.utils import EndOfMessage
+from src.locator.processors import (FileProducer, SQLJavaProcessorConsumer, SQLLCProcessorConsumer,
+                                    SQLDescProcessorConsumer)
 from src.locator.writer import FileWriter
-
-BRANCH = "master"  # default GitLab project branch
-SQL_REGEX = r"\"(\bselect\b|\bcall\b|\bupdate\b|\bdelete\b|\binsert\b|to_number|to_date|\bsubstr|\bnvl){1} (\s|\S|\n)*\";"
 
 """
 Script Entry Point
 @:arg GITLAB_URL   gitlab project url
 @:arg ACCESS_TOKEN project secret 
 """
+
+
+def search_sql_in_java():
+    # java processing
+    javaPipeline = queue.Queue()
+    javaFolders = queue.Queue()
+    if not config.get("section_JAVA", "JAVA_COUNT_FOLDER"):
+        javaFolders.put(config.get("section_JAVA", "JAVA_FOLDER_DEFAULT"))
+    else:
+        for i in range(1, int(config.get("section_JAVA", "JAVA_COUNT_FOLDER")) + 1):
+            javaFolders.put(config.get("section_JAVA", "JAVA_FOLDER_" + str(i)))
+    for i in range(1, 3):
+        fileProducer = FileProducer(project=project, commit=commit, pipeline=javaPipeline, folder=javaFolders,
+                                    regex=config.get("section_JAVA", "JAVA_MASK"))
+        fileProducer.start()
+    out_java = queue.Queue()
+    for i in range(1, 5):
+        javaConsumer = SQLJavaProcessorConsumer(project=project, pipeline=javaPipeline, out=out_java,
+                                                regex=config.get("section_REGEX", "JAVA_REGEX"), attempts=1)
+        javaConsumer.start()
+    fileWriter = FileWriter(pipeline=out_java, filePath="out_java.csv", attempts=4)
+    fileWriter.start()
+
+
+def search_sql_in_lc():
+    # xml processing (lc)
+    lcFolders = queue.Queue()
+    lcFolders.put(config.get("section_LC", "LC_FOLDER"))
+    lcPipeline = queue.Queue()
+    lcFileProducer = FileProducer(project=project, commit=commit, pipeline=lcPipeline, folder=lcFolders,
+                                  regex=config.get("section_LC", "LC_MASK"))
+    lcFileProducer.start()
+    out_lc = queue.Queue()
+    for i in range(1, 5):
+        lcFileProducer = SQLLCProcessorConsumer(project=project, pipeline=lcPipeline, out=out_lc, attempts=0)
+        lcFileProducer.start()
+    fileWriter = FileWriter(pipeline=out_lc, filePath="out_lc.csv", attempts=4)
+    fileWriter.start()
+
+
+def search_sql_in_edit():
+    # xml processing (edit)
+    editFolders = queue.Queue()
+    for i in range(1, int(config.get("section_DESC", "DESC_COUNT_FOLDER")) + 1):
+        editFolders.put(config.get("section_DESC", "FOLDER_" + str(i)))
+    editPipeline = queue.Queue()
+
+    editFileProducer = FileProducer(project=project, commit=commit, pipeline=editFolders, folder=editFolders,
+                                    regex=config.get("section_DESC", "EDIT_MASK"))
+    editFileProducer.start()
+    out_edit = queue.Queue()
+    for i in range(1, 5):
+        editFileConsumer = SQLDescProcessorConsumer(project=project, pipeline=editPipeline, out=out_edit, attempts=0)
+        editFileConsumer.start()
+    fileWriter = FileWriter(pipeline=out_edit, filePath="out_edit.csv", attempts=4)
+    fileWriter.start()
+
+
+def search_sql_in_table():
+    # xml processing (table-actions, fill, check)
+    descFolders = queue.Queue()
+    descFolders.put(config.get("section_DESC", "TABLE_FOLDER"))
+    for i in range(1, int(config.get("section_DESC", "DESC_COUNT_FOLDER")) + 1):
+        descFolders.put(config.get("section_DESC", "FOLDER_" + str(i)))
+    descPipeline = queue.Queue()
+    descFileProducer = FileProducer(project=project, commit=commit, pipeline=descPipeline, folder=descFolders,
+                                    regex=config.get("section_DESC", "TABLE_MASK"))
+    descFileProducer.start()
+    out_desc = queue.Queue()
+    for i in range(1, 5):
+        descFileConsumer = SQLDescProcessorConsumer(project=project, pipeline=descPipeline, out=out_desc, attempts=0)
+        descFileConsumer.start()
+    fileDescWriter = FileWriter(pipeline=out_desc, filePath="out_table.csv", attempts=4)
+    fileDescWriter.start()
+
+
+def search_sql_in_check():
+    # xml processing (table-actions, fill, check)
+    descFolders = queue.Queue()
+    for i in range(1, int(config.get("section_DESC", "DESC_COUNT_FOLDER")) + 1):
+        descFolders.put(config.get("section_DESC", "FOLDER_" + str(i)))
+    descPipeline = queue.Queue()
+    descFileProducer = FileProducer(project=project, commit=commit, pipeline=descPipeline, folder=descFolders,
+                                    regex=config.get("section_DESC", "CHECK_MASK"))
+    descFileProducer.start()
+    out_check = queue.Queue()
+    for i in range(1, 5):
+        descFileConsumer = SQLDescProcessorConsumer(project=project, pipeline=descPipeline, out=out_check, attempts=0)
+        descFileConsumer.start()
+    fileDescWriter = FileWriter(pipeline=out_check, filePath="out_check.csv", attempts=4)
+    fileDescWriter.start()
+
+
+def search_sql_in_fill():
+    # xml processing (table-actions, fill, check)
+    descFolders = queue.Queue()
+    for i in range(1, int(config.get("section_DESC", "DESC_COUNT_FOLDER")) + 1):
+        descFolders.put(config.get("section_DESC", "FOLDER_" + str(i)))
+    descPipeline = queue.Queue()
+    descFileProducer = FileProducer(project=project, commit=commit, pipeline=descPipeline, folder=descFolders,
+                                    regex=config.get("section_DESC", "FILL_MASK"))
+    descFileProducer.start()
+    out_fill = queue.Queue()
+    for i in range(1, 5):
+        descFileConsumer = SQLDescProcessorConsumer(project=project, pipeline=descPipeline, out=out_fill, attempts=0)
+        descFileConsumer.start()
+    fileDescWriter = FileWriter(pipeline=out_fill, filePath="out_fill.csv", attempts=4)
+    fileDescWriter.start()
+
+
+def search_sql_in_all_desc():
+    search_sql_in_edit()
+    search_sql_in_table()
+    search_sql_in_check()
+    search_sql_in_fill()
+
+
 if __name__ == '__main__':
     if argv.__len__() < 3:
         print("not enough parameters")
@@ -28,41 +143,41 @@ if __name__ == '__main__':
     if project is None:
         exit(1)
 
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("settings.ini")  # читаем конфиг
+
+    branch = config.get("section_MAIN", "BRANCH")
+
     # Hash code of the last commit from the master branch
-    commit = project.commits.list(ref_name=BRANCH)[0].id
-    # java processing
-    javaPipeline = queue.Queue()
-    javaFolders = queue.Queue()
-    javaFolders.put('Modules/asfk-autoproc/src/main/java')
-    javaFolders.put('Modules/asfk-convertation/src/main/java')
-    javaFolders.put('Modules/asfk-doc/src/main/java')
-    javaFolders.put('Modules/asfk-lib/src/main/java')
-    javaFolders.put('Modules/asfk-print/src/main/java')
-    javaFolders.put('Modules/asfk-ws/src/main/java')
-    javaFolders.put('Modules/sed-integration/src/main/java')
-    javaFolders.put('Modules/ws-client/src/main/java')
-    javaFolders.put('Modules/component-arpscroller')
-    for i in range(1, 3):
-        fileProducer = FileProducer(project=project, commit=commit, pipeline=javaPipeline, folderQueue=javaFolders,
-                                    regex=r"\.java$")
-        fileProducer.start()
+    commit = project.commits.list(ref_name=branch)[0].id
 
-    out = queue.Queue()
-    endOf = EndOfMessage(2)
-    for i in range(1, 5):
-        javaConsumer = SQLJavaProcessorConsumer(project=project, pipeline=javaPipeline, out=out, regex=SQL_REGEX, attempts=1)
-        javaConsumer.start()
-    # xml processing
-    xmlFolders = queue.Queue()
-    xmlFolders.put('Func/config/lifecycles')
-    xmlPipeline = queue.Queue()
-    xmlFileProducer = FileProducer(project=project, commit=commit, pipeline=xmlPipeline, folderQueue=xmlFolders,
-                                   regex=r"\.(lc|check|action)$")
-    xmlFileProducer.start()
-    for i in range(1, 5):
-        xmlFileConsumer = SQLXMLProcessorConsumer(project=project, pipeline=xmlPipeline, out=out, attempts=0)
-        xmlFileConsumer.start()
+    if config.get("section_MAIN", "JAVA_SEARCH") == 'true':
+        search_sql_in_java()
+    else:
+        print(f'поиск по java файлам пропускается')
 
-    fileWriter = FileWriter(pipeline=out, filePath="out.csv", attempts=8)
+    if config.get("section_MAIN", "LC_SEARCH") == 'true':
+        search_sql_in_lc()
+    else:
+        print(f'поиск по lc файлам пропускается')
 
-    fileWriter.start()
+    if config.get("section_MAIN", "ALL_DESC_SEARCH") == 'true':
+        search_sql_in_all_desc()
+    else:
+        print(f'общий поиск по edit/table-actions/fill/check файлам пропускается')
+        if config.get("section_MAIN", "EDIT_SEARCH") == 'true':
+            search_sql_in_edit()
+        else:
+            print(f'поиск по edit файлам пропускается')
+        if config.get("section_MAIN", "TABLE_SEARCH") == 'true':
+            search_sql_in_table()
+        else:
+            print(f'поиск по table-actions файлам пропускается')
+        if config.get("section_MAIN", "FILL_SEARCH") == 'true':
+            search_sql_in_fill()
+        else:
+            print(f'поиск по fill файлам пропускается')
+        if config.get("section_MAIN", "CHECK_SEARCH") == 'true':
+            search_sql_in_check()
+        else:
+            print(f'поиск по check файлам пропускается')
